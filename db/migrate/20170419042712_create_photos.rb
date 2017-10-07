1f967c70206ad8c693d1ae64e8a66d572f2eaa60
michael <mbostler@gmail.com>
2017-10-07 16:23:37 -0400

    migrations fix

--- db/migrate/20170419042712_create_photos.rb
+++ db/migrate/20170419042712_create_photos.rb
@@ -1,12 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[5.0]
-  def self.up
+  def change
     create_table :photos do |t|
       t.attachment :content
       t.integer :order
-    end
-  end
-
-  def self.down
-    remove_attachment :photos, :content
+    end    
   end
 end
